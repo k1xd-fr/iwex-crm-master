928e98b50d8489edeb1e17b91610a00c6d435425
sweetkurut <devxwc@gmail.com>
2023-07-10 16:43:27 +0600

    refresh inpit

--- frontend/src/components/Login/index.tsx
+++ frontend/src/components/Login/index.tsx
@@ -27,6 +27,7 @@ const Login = () => {
             name="password"
             required
             className={styles.input}
+            value={"хуйня"}
           />
           <span className={styles.highlight}></span>
           <span className={styles.bar}></span>
