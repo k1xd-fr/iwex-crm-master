48e147d77b3608cc377e3dc350d0a8f3b7d6718f
sweetkurut <devxwc@gmail.com>
2023-07-11 15:03:40 +0600

    add animation text

--- frontend/src/components/Home/index.tsx
+++ frontend/src/components/Home/index.tsx
@@ -1,26 +1,56 @@
+"use client";
 import Link from "next/link";
 import styles from "./home.module.sass";
 import Box from "@mui/material/Box";
 import TextField from "@mui/material/TextField";
+import { motion } from "framer-motion";
+
+const textAnimation = {
+  hidden: {
+    x: -100,
+    opacity: 0,
+  },
+  visible: (custom) => ({
+    x: 0,
+    opacity: 1,
+    transition: { delay: custom * 0.3 },
+  }),
+};
 
 const HomeComponent = () => {
   return (
-    <div className={styles.wrapper}>
-      <div className={styles.wrapper_container}>
-        <div className={styles.description_wrap}>
-          <h1 className={styles.title}> Добро пожаловать на Iwex CRM</h1>
-          <p className={styles.text}>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facere
-            necessitatibus reprehenderit fuga sint accusantium fugit nulla,
-            asperiores eligendi. Fugiat aliquam ex dignissimos reprehenderit
-            rerum!
-          </p>
-          <Link href="/login" className={styles.btn_link}>
-            Login
-          </Link>
+    <motion.section
+      initial="hidden"
+      whileInView="visible"
+      className={styles.container}
+    >
+      <div className={styles.wrapper}>
+        <div className={styles.wrapper_container}>
+          <div className={styles.description_wrap}>
+            <motion.h1
+              custom={1}
+              variants={textAnimation}
+              className={styles.title}
+            >
+              Добро пожаловать на Iwex CRM
+            </motion.h1>
+            <motion.p
+              custom={2}
+              variants={textAnimation}
+              className={styles.text}
+            >
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facere
+              necessitatibus reprehenderit fuga sint accusantium fugit nulla,
+              asperiores eligendi. Fugiat aliquam ex dignissimos reprehenderit
+              rerum!
+            </motion.p>
+            <Link href="/login" className={styles.btn_link}>
+              Login
+            </Link>
+          </div>
         </div>
       </div>
-    </div>
+    </motion.section>
   );
 };
 
