6353a2eb3dfd3a880d794a6fe3533dd6f7e01bf3
k1xd-fr <117137914+k1xd-fr@users.noreply.github.com>
2023-07-11 14:33:55 +0600

    fix animation logo

--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -1,41 +1,33 @@
 "use client";
-import { motion, useCycle } from "framer-motion";
+import { motion } from "framer-motion";
 import Link from "next/link";
 import styles from "./navigation.module.sass";
 
 const Header = () => {
   const text = "IWEX";
-  const [animationState, cycleAnimationState] = useCycle("scale", "rotate");
 
   const variants = {
-    scale: { scale: 1.1 },
-    rotate: { rotate: 360 },
-  };
-
-  const handleAnimationCycle = () => {
-    cycleAnimationState();
+    hidden: { opacity: 0, y: 20 },
+    visible: { opacity: 1, y: 0 },
   };
 
   return (
     <header className={styles.header}>
       <div className={styles.header__container}>
         <Link href="/">
-          <motion.a
-            className={styles.logo}
-            whileHover={animationState}
-            onClick={handleAnimationCycle}
-          >
+          <span className={styles.logo}>
             {text.split("").map((letter, index) => (
               <motion.span
                 key={index}
-                initial={{ opacity: 0, y: 20 }}
-                animate={{ opacity: 1, y: 0 }}
-                transition={{ duration: 0.5, delay: index * 0.1 }}
+                initial="hidden"
+                animate="visible"
+                variants={variants}
+                transition={{ delay: index * 0.1 }}
               >
                 {letter}
               </motion.span>
             ))}
-          </motion.a>
+          </span>
         </Link>
       </div>
     </header>
