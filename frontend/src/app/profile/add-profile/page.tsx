af24056aeae0bc6c1cd7e6122a2fdf80e53ce617
sweetkurut <devxwc@gmail.com>
2023-07-13 13:57:48 +0600

    add page and sidebar

---
+++ frontend/src/app/profile/add-profile/page.tsx
@@ -0,0 +1,8 @@
+import AddProfile from "@/components/Addprofile";
+import React from "react";
+
+const addProfile = () => {
+  return <AddProfile />;
+};
+
+export default addProfile;
---
+++ frontend/src/components/Addprofile/index.tsx
@@ -0,0 +1,11 @@
+import styles from "./addprofile.module.sass";
+
+const AddProfile = () => {
+  return (
+    <aside className={styles.sideBar}>
+      <h2>SideBar</h2>
+    </aside>
+  );
+};
+
+export default AddProfile;
