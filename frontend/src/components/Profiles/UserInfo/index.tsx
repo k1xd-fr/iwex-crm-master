0ad2a3f690ccc7ee7c06f0157b26386ddd6a3a98
k1xd-fr <117137914+k1xd-fr@users.noreply.github.com>
2023-07-14 14:21:23 +0600

    end:

--- frontend/src/components/Profiles/UserInfo/index.tsx
+++ frontend/src/components/Profiles/UserInfo/index.tsx
@@ -5,6 +5,16 @@ import { motion } from "framer-motion";
 
 import styles from "./style.module.sass";
 const UserInfo = () => {
+  const data = {
+    first__name: "Almaz",
+    last__name: "Junusov",
+    avatar: "/img/base__avatar.jpeg",
+    uni: "manas",
+    fac: "it-programming",
+    city: "Bishkek",
+    num: "996550599099",
+    dbth: "12.04.2004",
+  };
   return (
     <motion.div
       initial={{ opacity: 0 }}
@@ -19,37 +29,54 @@ const UserInfo = () => {
         </div>
         <hr style={{ height: "2px", background: "white", border: "none" }} />
         <div className={styles.body}>
-          <Image
-            src="/img/base__avatar.png"
-            alt="Avatar"
-            width={145}
-            height={150}
-          />
+          <div className={styles.avatar}>
+            <Image
+              src={data?.avatar ? data.avatar : "/img/base__avatar.png"}
+              alt={data?.avatar ? data.avatar : "/img/base__avatar.png"}
+              width={145}
+              height={150}
+              className={styles.avatar__img}
+            />
+          </div>
           <div className={styles.body__user}>
-            <h2 style={{ color: "#fff" }}>Almaz Junusov</h2>
+            <h2 style={{ color: "#fff" }}>
+              {data.first__name || data.last__name
+                ? data?.first__name + " " + data?.last__name
+                : "UserName"}
+            </h2>
             <div className={styles.body__user__block}>
               <div className={styles.info}>
                 <div className={styles.info__more}>
                   <span>Университет</span>
-                  <h3 style={{ color: "#fff" }}>Пример Университета</h3>
+                  <h3 style={{ color: "#fff" }}>
+                    {data?.uni ? data?.uni : "--"}
+                  </h3>
                 </div>
                 <div className={styles.info__more}>
                   <span>Факультет</span>
-                  <h3 style={{ color: "#fff" }}>Пример Факультета</h3>
+                  <h3 style={{ color: "#fff" }}>
+                    {data?.fac ? data?.fac : "--"}
+                  </h3>
                 </div>
                 <div className={styles.info__more}>
                   <span>Город</span>
-                  <h3 style={{ color: "#fff" }}> -- </h3>
+                  <h3 style={{ color: "#fff" }}>
+                    {data?.city ? data?.city : "--"}{" "}
+                  </h3>
                 </div>
               </div>
               <div className={styles.info}>
                 <div className={styles.info__more}>
                   <span>Номер</span>
-                  <h3 style={{ color: "#fff" }}> +996550599099 </h3>
+                  <h3 style={{ color: "#fff" }}>
+                    {data?.num ? "+" + data?.num : "--"}
+                  </h3>
                 </div>
                 <div className={styles.info__more}>
                   <span>Дата рождения</span>
-                  <h3 style={{ color: "#fff" }}> 12.03.2003 </h3>
+                  <h3 style={{ color: "#fff" }}>
+                    {data?.dbth ? data?.dbth : "--"}
+                  </h3>
                 </div>
               </div>
             </div>
